fd5a521135b151d388136a47f8e67abf61fed29b
Guillaume Goutaudier <guillaume.goutaudier@gmail.com>
2018-11-27 13:42:02 +0100

    scripts

---
+++ scripts/setenv_zh_peer2.sh
@@ -0,0 +1,5 @@
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/zurich.code4fun.com/users/Admin@zurich.code4fun.com/msp
+CORE_PEER_ADDRESS=peer2.zurich.code4fun.com:7051
+CORE_PEER_LOCALMSPID="ZurichMSP"
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/zurich.code4fun.com/peers/peer2.zurich.code4fun.com/tls/ca.crt
+
